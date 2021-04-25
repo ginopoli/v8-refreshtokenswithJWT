f8e17cb40ab85f67796436ee60623e1a57633459
mohamad <mohamad.lawand@outlook.com>
2021-04-25 16:45:58 +0100

    Fix on utc time

--- TodoApp/Controllers/AuthManagementController.cs
+++ TodoApp/Controllers/AuthManagementController.cs
@@ -310,7 +310,8 @@ namespace TodoApp.Controllers
         private DateTime UnixTimeStampToDateTime(long unixTimeStamp)
         {
             var dateTimeVal = new DateTime(1970, 1,1,0,0,0,0, DateTimeKind.Utc);
-            dateTimeVal = dateTimeVal.AddSeconds(unixTimeStamp).ToLocalTime();
+            dateTimeVal = dateTimeVal.AddSeconds(unixTimeStamp).ToUniversalTime();
+
             return dateTimeVal;
         }
 
